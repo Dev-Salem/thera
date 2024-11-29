f04eaf4eb7247a1d4af5e5c36cdbc73d9e9e8cb2
Dev-Salem <108498811+Dev-Salem@users.noreply.github.com>
2024-11-29 14:27:40 +0300

    add more packages

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:thera/src/core/app_theme.dart';
+import 'package:awesome_extensions/awesome_extensions.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -9,6 +10,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
       home: const HomeScreen(),
     );
   }
@@ -19,8 +21,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Hello world"),
+    return Scaffold(
+      body: const Text("Hello world").toCenter(),
     );
   }
 }
